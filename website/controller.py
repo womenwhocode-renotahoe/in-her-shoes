f334a2131f43a085061c6a7e990f5f11aea4a18e
Erin Keith <erinkeith.unr@gmail.com>
2015-08-28 19:25:17 -0700

    add virtual env and procfile for running on any platform

--- website/controller.py
+++ website/controller.py
@@ -5,8 +5,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def  index_page():
-    return render_template('index.html')
+def  hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
